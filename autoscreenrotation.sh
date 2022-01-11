c01252530c0277d032789b65e6a955cabdab79cb
Luca Leon Happel <luca.happel@uni-duesseldorf.de>
2022-01-11 01:44:23 +0100

    Do not disable keyboard and touchscreen on screen rotation

--- autoscreenrotation.sh
+++ autoscreenrotation.sh
@@ -8,27 +8,27 @@ function processnewcommand {
 	case $1 in
 		"Accelerometer orientation changed: normal")
 			echo "screen rotated to normal"
-			xinput --enable "MSFT0001:00 06CB:CE2D Touchpad"
-			xinput --enable "AT Translated Set 2 keyboard"
+			# xinput --enable "MSFT0001:00 06CB:CE2D Touchpad"
+			# xinput --enable "AT Translated Set 2 keyboard"
 			screenrotation.sh "normal"
 			;;
 		"Accelerometer orientation changed: bottom-up")
 			echo "screen rotated to inverted"
-			xinput --disable "MSFT0001:00 06CB:CE2D Touchpad"
-			xinput --disable "AT Translated Set 2 keyboard"
+			# xinput --disable "MSFT0001:00 06CB:CE2D Touchpad"
+			# xinput --disable "AT Translated Set 2 keyboard"
 			screenrotation.sh "inverted"
 			;;
 		"Accelerometer orientation changed: left-up")
 			echo "screen rotated to left"
 			screenrotation.sh "left"
-			xinput --disable "MSFT0001:00 06CB:CE2D Touchpad"
-			xinput --disable "AT Translated Set 2 keyboard"
+			# xinput --disable "MSFT0001:00 06CB:CE2D Touchpad"
+			# xinput --disable "AT Translated Set 2 keyboard"
 			;;
 		"Accelerometer orientation changed: right-up")
 			echo "screen rotated to right"
 			screenrotation.sh "right"
-			xinput --disable "MSFT0001:00 06CB:CE2D Touchpad"
-			xinput --disable "AT Translated Set 2 keyboard"
+			# xinput --disable "MSFT0001:00 06CB:CE2D Touchpad"
+			# xinput --disable "AT Translated Set 2 keyboard"
 			;;
 	esac
 }
