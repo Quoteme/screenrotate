511ebb014e05b2ecdbdd53d65bf76fc72374a50d
Luca Leon Happel <luca.happel@hhu.de>
2022-10-23 09:23:24 +0200

    save and restore keyboard backlight

--- disableInput.sh
+++ disableInput.sh
@@ -19,3 +19,9 @@ for id in $(xinput --list | sed -n '/.*Keyboard/s/.*=\([0-9]\+\).*/\1/p')
 do
   xinput --disable $id
 done
+
+#######################################
+# Save current keyboard brightness
+#######################################
+# TODO: Change this to a more generic solution
+brightnessctl --device="asus::kbd_backlight" get > /tmp/kbd_brightness_before_rotation
--- enableInput.sh
+++ enableInput.sh
@@ -3,6 +3,9 @@
 # @AUTHOR: Luca Leon Happel
 #
 
+#######################################
+# Enable keyboard and touchpad
+#######################################
 echo "Enabling keyboard and touchpad"
 for id in $(xinput --list | sed -n '/.*Touchpad/s/.*=\([0-9]\+\).*/\1/p')
 do
@@ -16,3 +19,10 @@ for id in $(xinput --list | sed -n '/.*Keyboard/s/.*=\([0-9]\+\).*/\1/p')
 do
   xinput --enable $id
 done
+
+#######################################
+# Resore last keyboard brightness
+#######################################
+if test -f "/tmp/kbd_brightness_before_rotation"; then
+  brightnessctl --device="asus::kbd_backlight" set $(cat /tmp/kbd_brightness_before_rotation)
+fi
